9424212cbfb8cbce2a67e10c770ad85eda09e8ec
Karl T. Macadangdang <karljarren0308@gmail.com>
2018-04-05 07:01:59 +0800

    Added some constant variables

--- backend/database.php
+++ backend/database.php
@@ -1,6 +1,11 @@
 <?php
     date_default_timezone_set('Asia/Manila');
 
-    $connection = mysqli_connect('localhost', 'root', '', 'ppfms_db');
+    define('DB_HOSTNAME', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_NAME', 'ppfms_db');
+
+    $connection = mysqli_connect(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_NAME);
     $today = date('Y-m-d H:i:s');
 ?>
