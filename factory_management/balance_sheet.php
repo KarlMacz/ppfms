bb07529499fc5bd907067bb7bb70326c93d6c5ea
Karl T. Macadangdang <karljarren0308@gmail.com>
2018-04-06 00:41:40 +0800

    Initial commit

---
+++ factory_management/balance_sheet.php
@@ -0,0 +1,41 @@
+<?php
+    session_start();
+
+    require_once('../backend/database.php');
+    require_once('../backend/functions.php');
+
+    include_once('../layouts/authorized_header.php');
+    include_once('../layouts/factory_management_start.php');
+?>
+<nav class="navbar navbar-default navbar-static-top no-margin">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <div class="navbar-brand"><span class="fas fa-balance-scale fa-fw"></span> Balance Sheet</div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $_SESSION['full_name']; ?> <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                    <li><a href="profile.php">Profile</a></li>
+                    <li class="divider"></li>
+                    <li><a href="../logout.php">Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div id="page-wrapper-content">
+</div>
+<?php
+    include_once('../layouts/factory_management_end.php');
+    include_once('../partials/modals.php');
+    include_once('../layouts/footer.php');
+?>
