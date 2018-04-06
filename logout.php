204612646ea5e0cbb97fcdf58b21cb36f74fecab
Karl T. Macadangdang <karljarren0308@gmail.com>
2018-04-07 04:28:14 +0800

    Added audit trail

--- logout.php
+++ logout.php
@@ -1,6 +1,10 @@
 <?php
     session_start();
 
+    $accountID = $_SESSION['user_id'];
+
+    mysqli_query($connection, "INSERT INTO `logs` (`acccount_id`, `message`, `created_at`) VALUES ('$accountID', 'has logged out.', '$today')");
+
     session_destroy();
 
     header('Location: login.php');
