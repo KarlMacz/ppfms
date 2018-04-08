286d28610f1cf389f445f576e775c9ff744c6e34
Karl T. Macadangdang <karljarren0308@gmail.com>
2018-04-08 10:13:34 +0800

    Updated to add finished batch to the product's inventory

--- backend/ajax/mark_as_finished_batch.php
+++ backend/ajax/mark_as_finished_batch.php
@@ -7,19 +7,37 @@
     if($_SERVER['REQUEST_METHOD'] === 'POST') {
         $id = input_escape_string($connection, $_POST['id']);
 
-        $query = mysqli_query($connection, "UPDATE `batches` SET `status`='Finished' WHERE `id`='$id'");
+        $batchQuery = mysqli_query($connection, "SELECT * FROM `batches` WHERE `id`='$id'");
 
-        if(mysqli_affected_rows($connection) === 1) {
-            echo json_encode([
-                'status' => 'Ok',
-                'type' => 'prompt',
-                'message' => 'Batch has been marked as finished.'
-            ]);
+        if(mysqli_num_rows($batchQuery) === 1) {
+            $batchRow = mysqli_fetch_assoc($batchQuery);
+            $productID = $batchRow['product_id'];
+            $quantity = $batchRow['quantity'];
+            $price = $batchRow['batch_price'];
+
+            $query = mysqli_query($connection, "UPDATE `batches` SET `status`='Finished' WHERE `id`='$id'");
+
+            if(mysqli_affected_rows($connection) === 1) {
+                mysqli_query($connection, "INSERT INTO `inventories` (`product_id`, `batch_id`, `boxes_arrived`, `boxes_in_stock`, `price`)
+                    VALUES ('$productID', '$id', '$quantity', '$quantity', '$price')");
+
+                echo json_encode([
+                    'status' => 'Ok',
+                    'type' => 'prompt',
+                    'message' => 'Batch has been marked as finished.'
+                ]);
+            } else {
+                echo json_encode([
+                    'status' => 'Error',
+                    'type' => 'prompt',
+                    'message' => 'Failed to mark batch as finished.'
+                ]);
+            }
         } else {
             echo json_encode([
                 'status' => 'Error',
                 'type' => 'prompt',
-                'message' => 'Failed to mark batch as finished.'
+                'message' => 'Product doesn\'t exist.'
             ]);
         }
     } else {
