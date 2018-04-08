0062032952d449dc383c7d47cb2db5baf35af613
Karl T. Macadangdang <karljarren0308@gmail.com>
2018-04-08 20:27:36 +0800

    Added timestamp for dispatch

--- backend/ajax/dispatch_order.php
+++ backend/ajax/dispatch_order.php
@@ -7,7 +7,7 @@
     if($_SERVER['REQUEST_METHOD'] === 'POST') {
         $id = input_escape_string($connection, $_POST['id']);
 
-        $query = mysqli_query($connection, "UPDATE `orders` SET `status`='Dispatched' WHERE `id`='$id'");
+        $query = mysqli_query($connection, "UPDATE `orders` SET `status`='Dispatched', `timestamp_dispatched`='$today' WHERE `id`='$id'");
 
         if(mysqli_affected_rows($connection) === 1) {
             echo json_encode([
