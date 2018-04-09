5a40bfb22ee35b425ce58572e238f2630d2fd2b1
Karl T. Macadangdang <karljarren0308@gmail.com>
2018-04-09 09:08:19 +0800

    Added function for restoring default settings

--- backend/database.php
+++ backend/database.php
@@ -9,4 +9,32 @@
 
     $connection = mysqli_connect(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_NAME);
     $today = date('Y-m-d H:i:s');
+
+    function restoreSettings() {
+        global $connection;
+
+        $settings = [
+            'shipping_fee_within_metro_manila',
+            'shipping_fee_outside_metro_manila',
+            'critical_level'
+        ]
+
+        $settingsQuery = mysqli_query($connection, "SELECT * FROM `settings`");
+
+        if(mysqli_num_rows($settingsQuery) > 0) {
+            while($settingsRow = mysqli_fetch_assoc($settingsQuery)) {
+                $index = array_search($settingsRow['name'], $settings);
+
+                if($index !== false) {
+                    unset($settings[$index]);
+                }
+            }
+        }
+
+        foreach($settings as $setting) {
+            mysqli_query($connection, "INSERT INTO `settings` (`name`, `value`) VALUES ('$setting', '1')")
+        }
+    }
+
+    restoreSettings();
 ?>
